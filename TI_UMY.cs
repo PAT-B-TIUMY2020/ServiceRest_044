80fe2b2f0a7b04d60e6c1faf8d5c8d8d2a85c300
Fakhrizar Rifqi Fuad <fakhrizar.rifqi.ft18@mail.umy.ac.id>
2020-12-19 19:35:07 +0700

    create service WCF Rest

---
+++ TI_UMY.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ServiceRest_20180140044_FakhrizarRifqiFuad
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class TI_UMY : ITI_UMY
+    {
+        public string CreateMahasiswa(Mahasiswa mhs)
+        {
+            string msg = "GAGAL";
+            SqlConnection sqlcon = new SqlConnection("Data Source=LAPTOP-N38C71DV; Initial Catalog=\"TI UMY\"; Persist Security Info=True; User ID=sa; Password=Kawan123");
+            string query = String.Format("Insert into dbo.Mahasiswa values ('{0}', '{1}', '{2}', '{3}')", mhs.nim, mhs.nama, mhs.prodi, mhs.angkatan);
+            SqlCommand sqlcom = new SqlCommand(query, sqlcon); //yang dikirim ke sql
+
+            try
+            {
+                sqlcon.Open(); //membuka connection sql
+
+                Console.WriteLine(query);
+
+                sqlcom.ExecuteNonQuery(); //mengeksekusi untuk memasukkan data
+
+                sqlcon.Close();
+
+                msg = "Sukses";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+                msg = "GAGAL";
+            }
+
+            return msg;
+        }
+
+        public List<Mahasiswa> GetAllMahasiswa()
+        {
+            List<Mahasiswa> mahas = new List<Mahasiswa>();
+
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-N38C71DV; Initial Catalog=\"TI UMY\"; Persist Security Info=True; User ID=sa; Password=Kawan123");
+            string query = "select NIM, Nama, Prodi, Angkatan from dbo.Mahasiswa";
+            SqlCommand com = new SqlCommand(query, con); // yang dikirim ke sql
+
+            try
+            {
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader(); //mendapatkan data yang telah dieksekusi, dari select. Hasil query ditaruh di reader
+
+                while (reader.Read())
+                {
+                    Mahasiswa mhs = new Mahasiswa();
+                    mhs.nim = reader.GetString(0); // 0 itu array pertama // ini diambil dari IService
+                    mhs.nama = reader.GetString(1);
+                    mhs.prodi = reader.GetString(2);
+                    mhs.angkatan = reader.GetString(3);
+
+                    mahas.Add(mhs);
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return mahas; // output
+        }
+
+        public Mahasiswa GetMahasiswaByNIM(string nim)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-N38C71DV; Initial Catalog=\"TI UMY\"; Persist Security Info=True; User ID=sa; Password=Kawan123");
+            string query = String.Format("select NIM, Nama, Prodi, Angkatan from dbo.Mahasiswa where NIM = '{0}'", nim);
+            SqlCommand com = new SqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader(); //mendapatkan data yang telah dieksekusi, dari select. Hasil query ditaruh di reader
+
+                while (reader.Read())
+                {
+                    mhs.nim = reader.GetString(0); // 0 itu array pertama // ini diambil dari IService
+                    mhs.nama = reader.GetString(1);
+                    mhs.prodi = reader.GetString(2);
+                    mhs.angkatan = reader.GetString(3);
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return mhs;
+        }
+    }
+}
